93c596c92cb21f66011d8bbd70240275ec479082
KanaFujitani <cattleya.leya0@gmail.com>
2023-05-25 21:11:57 +0900

    add: files

---
+++ TrainWindowGuide/ResultsDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsDetailViewController.swift
+//  TrainWindowGuide
+//
+//  Created by Kana Fujitani on 2023/05/24.
+//
+
+import UIKit
+
+class ResultsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
