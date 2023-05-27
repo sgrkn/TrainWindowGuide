ea0894f5b7dcd0274de6e091c7f8c1ce61d988b3
KanaFujitani <cattleya.leya0@gmail.com>
2023-05-27 19:23:33 +0900

    add: new files

---
+++ TrainWindowGuide/BookmarkViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookmarkViewController.swift
+//  TrainWindowGuide
+//
+//  Created by Kana Fujitani on 2023/05/27.
+//
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TrainWindowGuide/SearchResultsViewController.swift
+++ TrainWindowGuide/SearchResultsViewController.swift
@@ -6,24 +6,78 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class SearchResultsViewController: UIViewController {
+class SearchResultViewController: UIViewController, CLLocationManagerDelegate {
+    var myLock = NSLock()
+    let goldenRatio = 1.618
+    @IBOutlet var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    @IBAction func clickZoomin(_ sender: Any) {
+        print("[DBG]clickZoomin")
+        myLock.lock()
+        if (0.005 < mapView.region.span.latitudeDelta / goldenRatio) {
+            print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / goldenRatio
+            mapView.region.span = regionSpan
+            print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
+        }
+        myLock.unlock()
+        
+    }
+    
+    @IBAction func clickZoomout(_ sender: Any) {
+        print("[DBG]clickZoomout")
+        myLock.lock()
+        if (mapView.region.span.latitudeDelta * goldenRatio < 150.0) {
+            print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * goldenRatio
+         // regionSpan.latitudeDelta = mapView.region.span.longitudeDelta * GoldenRatio
+            mapView.region.span = regionSpan
+            print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
+        }
+        myLock.unlock()
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()  // 変数を初期化
+        locationManager.delegate = self  // delegateとしてself(自インスタンス)を設定
+        
+        locationManager.startUpdatingLocation()  // 位置情報更新を指示
+        locationManager.requestWhenInUseAuthorization()  // 位置情報取得の許可を得る
+        
+        mapView.showsUserLocation = true
+        
+        // ピンを立てる
+        // ピンを立てたい緯度・経度をセット
+        let coordinate = CLLocationCoordinate2DMake(36.3922466621714, 139.057872435089)
+        // 現在地の場合
+        // let coordinate = mapView.userLocation.coordinate
+        // ピンを生成
+        let pin = MKPointAnnotation()
+        // ピンのタイトル・サブタイトルをセット
+        pin.title = "利根川"
+        pin.subtitle = "流域面積日本一の河川"
+        // ピンに一番上で作った位置情報をセット
+        pin.coordinate = coordinate
+        // mapにピンを表示する
+        mapView.addAnnotation(pin)
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    func locationManager2(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        let longitude = (locations.last?.coordinate.longitude.description)!
+        let latitude = (locations.last?.coordinate.latitude.description)!
+        // print("[DBG]longitude : " + longitude)
+        // print("[DBG]latitude : " + latitude)
 
-}
+    }
+
