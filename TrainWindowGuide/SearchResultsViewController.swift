e35c017cf08413fa105f7bae9ed0280e59e9e12f
KanaFujitani <cattleya.leya0@gmail.com>
2023-06-09 21:54:41 +0900

    feat: 位置情報取得と初期位置修正・検索機能追加

--- TrainWindowGuide/SearchResultsViewController.swift
+++ TrainWindowGuide/SearchResultsViewController.swift
@@ -15,7 +15,9 @@ class SearchResultsViewController: UIViewController, CLLocationManagerDelegate, 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager!
     @IBOutlet weak var inputText: UITextField!
-      
+    
+    var initialLocationSet = false // 初期位置が設定されたかどうかを追跡するフラグ
+    
     @IBAction func clickZoomin(_ sender: Any) {
         print("[DBG]clickZoomin")
         myLock.lock()
@@ -68,7 +70,7 @@ class SearchResultsViewController: UIViewController, CLLocationManagerDelegate, 
             let longitude = pin.longitude
             let latitude = pin.latitude
             let coordinate = CLLocationCoordinate2DMake(longitude, latitude)
-
+            
             // ピンを生成
             let pinAnnotation = MKPointAnnotation()
             // ピンのタイトル・サブタイトルをセット
@@ -79,14 +81,14 @@ class SearchResultsViewController: UIViewController, CLLocationManagerDelegate, 
             // mapにピンを表示する
             mapView.addAnnotation(pinAnnotation)
         }
-
+        
     }
     
     // ピンがタップされたときの処理
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         performSegue(withIdentifier: "toDetail", sender: view.annotation)
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDetail" {
             let next = segue.destination
@@ -106,58 +108,126 @@ class SearchResultsViewController: UIViewController, CLLocationManagerDelegate, 
             }
         }
     }
-
     
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
-//        let longitude = (locations.last?.coordinate.longitude.description)!
-//        let latitude = (locations.last?.coordinate.latitude.description)!
-//        // print("[DBG]longitude : " + longitude)
-//        // print("[DBG]latitude : " + latitude)
+    
+    //    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+    //        let longitude = (locations.last?.coordinate.longitude.description)!
+    //        let latitude = (locations.last?.coordinate.latitude.description)!
+    //        // print("[DBG]longitude : " + longitude)
+    //        // print("[DBG]latitude : " + latitude)
+    //
+    //    }
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let currentLocation = locations.last else { return }
+//        let coordinate = currentLocation.coordinate
 //
+//        // 現在地を地図の表示範囲に反映させる
+//        let currentRegion = MKCoordinateRegion(center: coordinate, span: mapView.region.span)
+//        mapView.setRegion(currentRegion, animated: true)
 //    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//            guard let currentLocation = locations.last else { return }
+//
+//            if !initialLocationSet { // 初期位置がまだ設定されていない場合のみ処理を行う
+//                let coordinate = currentLocation.coordinate
+//
+//                // 現在地を地図の表示範囲に反映させる
+//                let currentRegion = MKCoordinateRegion(center: coordinate, span: mapView.region.span)
+//                mapView.setRegion(currentRegion, animated: true)
+//
+//                initialLocationSet = true // 初期位置が設定されたことをマークする
+//            }
+//        }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let currentLocation = locations.last else { return }
-        let coordinate = currentLocation.coordinate
-        
-        // 現在地を地図の表示範囲に反映させる
-        let currentRegion = MKCoordinateRegion(center: coordinate, span: mapView.region.span)
-        mapView.setRegion(currentRegion, animated: true)
-    }
-
-    
-    
-    
-    // 検索機能
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            textField.resignFirstResponder()
-
-            if let searchKey = textField.text {
-
-            print(searchKey)
-
-            let geocoder = CLGeocoder()
-
-                geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
-
-                    if let unwrapPlacemarks = placemarks {
-                        if let firstPlacemark = unwrapPlacemarks.first {
-                            if let location = firstPlacemark.location {
-                                let targetCoordinate = location.coordinate
-                                print(targetCoordinate)
-
-                                let pin = MKPointAnnotation()
-
-                                pin.coordinate = targetCoordinate
-                                pin.title = searchKey
-                                self.mapView.addAnnotation(pin)
-
-                                self.mapView.region = MKCoordinateRegion(center: targetCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
-                            }
-                        }
-                    }
-                })
+            guard let currentLocation = locations.last else { return }
+            
+            if !initialLocationSet {
+                let coordinate = currentLocation.coordinate
+                let currentRegion = MKCoordinateRegion(center: coordinate, span: mapView.region.span)
+                mapView.setRegion(currentRegion, animated: true)
+                initialLocationSet = true // 初期位置が設定されたことをマークする
+            }
         }
-        return true
+        
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // 位置情報の取得が許可されていない場合の初期位置を指定する
+        let initialCoordinate = CLLocationCoordinate2D(latitude: 35.6895, longitude: 139.6917)
+        // 例として東京の座標を使用
+        let initialRegion = MKCoordinateRegion(center: initialCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+        mapView.setRegion(initialRegion, animated: true)
+            
+            initialLocationSet = true
+        }
+    
+    
+    
+    //    // 検索機能
+    //    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    //            textField.resignFirstResponder()
+    //
+    //            if let searchKey = textField.text {
+    //
+    //            print(searchKey)
+    //
+    //            let geocoder = CLGeocoder()
+    //
+    //                geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+    //
+    //                    if let unwrapPlacemarks = placemarks {
+    //                        if let firstPlacemark = unwrapPlacemarks.first {
+    //                            if let location = firstPlacemark.location {
+    //                                let targetCoordinate = location.coordinate
+    //                                print(targetCoordinate)
+    //
+    //                                let pin = MKPointAnnotation()
+    //
+    //                                pin.coordinate = targetCoordinate
+    //                                pin.title = searchKey
+    //                                self.mapView.addAnnotation(pin)
+    //
+    //                                self.mapView.region = MKCoordinateRegion(center: targetCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+    //                            }
+    //                        }
+    //                    }
+    //                })
+    //        }
+    //        return true
+    //    }
+    //
+    
+    // 検索機能・フィルター
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+
+        if let searchKey = textField.text {
+            if let searchKey = textField.text {
+                let filteredPins = PinData.DEFAULT_DATAS.filter { pin in
+                    if var title: String = pin.title, title.lowercased().contains(searchKey.lowercased()) {
+                        return true
+                    }
+                    return false
+                }
+
+                for pin in filteredPins {
+                    let coordinate = CLLocationCoordinate2D(latitude: pin.latitude, longitude: pin.longitude)
+                    let pinAnnotation = MKPointAnnotation()
+                    pinAnnotation.coordinate = coordinate
+                    pinAnnotation.title = pin.title
+                    mapView.addAnnotation(pinAnnotation)
+                }
+
+                if let firstPin = filteredPins.first {
+                    let coordinate = CLLocationCoordinate2D(latitude: firstPin.latitude, longitude: firstPin.longitude)
+                    let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                    mapView.setRegion(region, animated: true)
+                }
+            }
+
+
+            return true
+        }
+
     }
 }
