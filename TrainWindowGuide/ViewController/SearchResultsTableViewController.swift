69ef04276dff0e9e4398812d050b60c49873b5c1
KanaFujitani <cattleya.leya0@gmail.com>
2023-06-16 16:27:48 +0900

    add: PinDataの定義

---
+++ TrainWindowGuide/ViewController/SearchResultsTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SearchResultsTableViewController.swift
+//  TrainWindowGuide
+//
+//  Created by Kana Fujitani on 2023/06/16.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UIViewController {
+    
+    var pinDatas: [PinData] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SearchResultsTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
