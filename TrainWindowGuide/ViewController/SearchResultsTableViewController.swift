16bb9d56f5f91446d873c0898675374f7135b71e
KanaFujitani <cattleya.leya0@gmail.com>
2023-06-18 02:20:40 +0900

    feat: 周辺検索結果一覧実装

--- TrainWindowGuide/ViewController/SearchResultsTableViewController.swift
+++ TrainWindowGuide/ViewController/SearchResultsTableViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 class SearchResultsTableViewController: UIViewController {
     
     var pinDatas: [PinData] = []
+    
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.register(UINib(nibName: "SearchResultsTableViewCell", bundle: nil), forCellReuseIdentifier: "ResultsCell")
+        tableView.dataSource = self
+        // print(pinDatas)
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -31,11 +34,12 @@ class SearchResultsTableViewController: UIViewController {
 
 extension SearchResultsTableViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        pinDatas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultsCell", for: indexPath) as! SearchResultsTableViewCell
+                return cell
     }
     
     
